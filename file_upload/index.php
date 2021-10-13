ef6d0b566d179abe84f453021e6500d15ea469c2
Wanderson <wandersonfelipeplus13@gmail.com>
2021-10-13 18:26:02 -0300

    file upload

---
+++ file_upload/index.php
@@ -0,0 +1,35 @@
+<html>
+    <body>
+
+        <?php
+        
+        if(isset($_POST['send'])):
+            $formats = array("png", "jpeg", "jpg", "gif");
+            $extension = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
+
+            if(in_array($extension, $formats)):
+                $dir = "files/";
+                $tmp = $_FILES['file']['tmp_name'];
+                $new_name = uniqid().".$extension";
+
+                if(move_uploaded_file($tmp, $dir.$new_name)):
+                    $message = "Successfully uploaded!";
+                else:
+                    $message = "Unable to upload!";
+                endif;
+            else:
+                $message = "Invalid format!";
+            endif;
+
+            echo $message;
+        endif;
+
+        ?>
+
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
+            <input type="file" name="file"><br>
+            <input type="submit" name="send">
+        </form>
+    </body>
+</html>
+
